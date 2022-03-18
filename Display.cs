d86496a0ef994022d58cabe6d8fc1336943f0686
Caleb Wightman <agentcboy@gmail.com>
2022-03-18 17:12:58 -0600

    Display Class Implemented

--- Display.cs
+++ Display.cs
@@ -2,6 +2,36 @@
 {
     public class Display
     {
+        List<DrawPoint> nextFrame;
+        List<DrawPoint> currentFrame;
+        int height, width;
+
+        public Display(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        
+        public void DrawNextFrame(List<IDrawable> objects)
+        {
+            var tempList = new List<DrawPoint>();
+            foreach (IDrawable obj in objects)
+            {
+                foreach (DrawPoint point in obj.DrawPoints)
+                {
+                    nextFrame.Add(point);
+                }
+            }
+        }
+        public void PrintCurrentFrame()
+        {
+            
+                foreach (var point in nextFrame)
+                {
+                    Console.SetCursorPosition(point.X, this.height - point.Y);
+                    Console.Write(point.Character);
+                }
+        }
 
     }
 }
