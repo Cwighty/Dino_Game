93fc96e7e4795544a3c2589e11563c0e815abfaf
andrewbengordon <andrewbengordon@gmail.com>
2022-03-18 19:37:28 -0600

    Bird Animation

--- Game.cs
+++ Game.cs
@@ -13,21 +13,34 @@
             var objectsToDraw = new List<IDrawable>();
             //TODO: add your cactus/birds/dinos to the collisionObject list to print them out
             // You can create an object at the position you want it : See the bird class for how to implement IDrawable
-            //var bird = new Bird(displayWidth, 10, 1, 2);
-            //objectsToDraw.Add(bird);
+            var bird = new Bird(displayWidth, 10, 1, 2);
+            objectsToDraw.Add(bird);
 
             while(true)
             {
+                objectsToDraw = CheckVisiblity(objectsToDraw);
                 Console.Clear();
                 display.DrawNextFrame(objectsToDraw);
                 display.PrintCurrentFrame();
                 Thread.Sleep(4000/gameSpeed);
                 //You can move you object here
-                //bird.moveLeft();
+                bird.moveLeft();
 
                 score++;
             }
 
+        }
+        public static List<IDrawable> CheckVisiblity(List<IDrawable> otd)
+        {
+            List<IDrawable> visiblity = new List<IDrawable>();
+            foreach (IDrawable obj in otd)
+            {
+                if (obj.IsVisible)
+                {
+                    visiblity.Add(obj);
+                }
+            }
+            return visiblity;
         }
     }
 }
