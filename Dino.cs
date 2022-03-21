3e1b27184c0a51fe583fb0928c20f2867e46d8c2
Kolin Nielson <kolin.nielson@students.snow.edu>
2022-03-21 11:43:37 -0600

    dino class without full functionality

--- Dino.cs
+++ Dino.cs
@@ -4,6 +4,22 @@
     {
         public Dino(int x, int y, int height, int width) : base(x, y, height, width)
         {
+            this.IsVisible = true;
+        }
+        public char Head { get; set; }
+        public char Body { get; set; }
+        public bool isInAir { get; set; }
+
+
+        public void Jump()
+        {
+            //at spacebar or up arrowkey press:
+
+            isInAir = true;
+
+            //update head and body postions for jump period
+
+            isInAir = false;
         }
     }
 }
